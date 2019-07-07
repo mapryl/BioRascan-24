d86c92c0bd171754eb7fc82f7cf99a33f46f9720
frank_goods <terenty.rezman@mail.ru>
2019-07-07 21:36:35 +0300

    add sound when closed add auto-save

--- qt/main.py
+++ qt/main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QGridLayout, QHBoxLa
     QCheckBox, QFileDialog, QSpacerItem, QMessageBox)
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtMultimedia import QSoundEffect
 
 from SerialPortReader import *
 from SerialPortWriter import *
@@ -10,6 +11,7 @@ import pyqtgraph as pg
 import numpy as np
 
 from BreathingRateCounter import breath_rate_counter
+from pip._internal import exceptions
 
 class RascanWorker(QObject):
 
@@ -32,13 +34,15 @@ class RascanWorker(QObject):
         self.dataProcessed.emit(hr, br)
 
 class PlotWidget(pg.GraphicsWindow):
-    def __init__(self):
+    def __init__(self, maxY):
         super(self.__class__, self).__init__(None)
 
         self.setWindowTitle('pyqtgraph example: Scrolling Plots')
 
         p = self.addPlot()
-        self.data = np.zeros(30)
+        self.plot = p
+        self.dataSize = 10
+        self.data = []
         self.curve = p.plot(self.data, pen=pg.mkPen(QColor('#5961FF'), width=2))
         self.ptr = 0
 
@@ -49,31 +53,62 @@ class PlotWidget(pg.GraphicsWindow):
         p.getAxis("left").tickFont = font
         p.getAxis("left").setStyle(tickTextOffset=30)
 
-        #self.timer = QTimer()
-        #self.timer.timeout.connect(self.update)
-        #self.timer.start(250)
-
-    def update(self):
-        self.appendPoint(np.random.normal())
+        p.setYRange(0, maxY, padding = 0)
 
     def appendPoint(self, value):
-        self.data[:-1] = self.data[1:]
-        self.data[-1] = value
+        if(len(self.data) < self.dataSize):
+            self.data.append(value)
+        else:
+            self.data[:-1] = self.data[1:]
+            self.data[-1] = value
 
-        self.ptr += 1
+            self.ptr += 1
+        self.curve.setData(self.data)
+        self.curve.setPos(self.ptr, 0)
+
+    def reset(self):
+        self.data = [0]
+        self.ptr = 0
+        self.curve.setData(self.data)
+        self.curve.setPos(self.ptr, 0)
+        self.data = []
+        self.ptr = 0
         self.curve.setData(self.data)
         self.curve.setPos(self.ptr, 0)
 
 class ExperimentData:
     def __init__(self):
-        self.a_ch0 = []
-        self.a_ch1 = []
-        self.T_meas = []
+        self.reset()
 
     def append(self, a_ch0, a_ch1, T_meas):
         self.a_ch0 += a_ch0
         self.a_ch1 += a_ch1
         self.T_meas += T_meas
+        self.needToSave = True
+
+    def reset(self):
+        self.a_ch0 = []
+        self.a_ch1 = []
+        self.T_meas = []
+        self.needToSave = False
+
+    def saveIfNeeded(self):
+        if(self.needToSave):
+            self.saveToFile()
+
+    def saveToFile(self):
+        if(len(self.T_meas) != 0):
+            fileName = QFileDialog.getSaveFileName(None, "Save unsaved data to file", QDir(".").canonicalPath(), "NPZ(*.npz)")
+            np.savez_compressed(fileName[0], ch0=self.a_ch0, ch1=self.a_ch1,
+                                T=self.T_meas)
+
+class MyIntValidator(QIntValidator):
+    def __init__(self, min, max):
+        super(self.__class__, self).__init__(min, max)
+        self.min = min
+
+    def fixup(self, s):
+        return str(self.min)
 
 class MainWindow(QWidget):
     processData = pyqtSignal(list, list, int)
@@ -85,11 +120,14 @@ class MainWindow(QWidget):
         self.experimentData = ExperimentData()
 
         self.initGUI()
+        self.initSound()
 
         self.createWorkerThread()
 
-        self.plotWidget = PlotWidget()
-        self.rightLayout.addWidget(self.plotWidget)
+        self.heartRatePlotWidget = PlotWidget(70)
+        self.breathRatePlotWidget = PlotWidget(40)
+        self.rightLayout.addWidget(self.heartRatePlotWidget)
+        self.rightLayout.addWidget(self.breathRatePlotWidget)
 
         self.reader.timeUpdate.connect(self.onTimeUpdate)
         self.reader.dataReady.connect(self.onDataReady)
@@ -101,8 +139,14 @@ class MainWindow(QWidget):
 
     @QtCore.pyqtSlot(int)
     def onTimeUpdate(self, time):
-        time = QTime.fromMSecsSinceStartOfDay(time)
-        self.timeLabel.setText(time.toString())
+        time = self.experimentLength*60*1000 - time
+        qtTime = QTime.fromMSecsSinceStartOfDay(time)
+        self.timeLabel.setText(qtTime.toString())
+
+        if(time == 0):
+            self.startStopButton.toggle()
+            if(self.soundCheckBox.isChecked()):
+                self.sound.play()
 
     def createWorkerThread(self):
         self.rascanWorker = RascanWorker()
@@ -112,6 +156,14 @@ class MainWindow(QWidget):
 
         self.processData.connect(self.rascanWorker.doWork)
         self.rascanWorker.dataProcessed.connect(self.onRascanDataProcessed)
+
+    def initSound(self):
+        self.sound = QSoundEffect()
+        base_url = QUrl.fromLocalFile(QDir(".").canonicalPath() + "/")
+        file_path = base_url.resolved(QUrl("bzzz.wav"))
+
+        self.sound.setSource(file_path)
+        self.sound.setVolume(1)
 
     def initGUI(self):
         self.setWindowTitle('БиоРАСКАН-24')
@@ -135,32 +187,31 @@ class MainWindow(QWidget):
         settingsLayout = QGridLayout()
         leftLayout.addLayout(settingsLayout)
 
-        validator = QIntValidator(1, 60)
         lengthSettingsText = QLabel('Длительность эксперимента')
-        lengthSettingsEdit = QLineEdit('1')
-        lengthSettingsEdit.setValidator(validator)
+        self.lengthSettingsEdit = QLineEdit('1')
+        self.lengthSettingsEdit.setValidator(MyIntValidator(1, 30))
         lmin = QLabel('мин')
         lmin.setObjectName("secondary")
 
         settingsLayout.addWidget(lengthSettingsText, 0, 0)
-        settingsLayout.addWidget(lengthSettingsEdit, 0, 1)
+        settingsLayout.addWidget(self.lengthSettingsEdit, 0, 1)
         settingsLayout.addWidget(lmin, 0, 2)
 
         intervalLayoutText = QLabel('Интервал расчета')
-        self.intervalLayoutEdit = QLineEdit('1')
-        self.intervalLayoutEdit.setValidator(validator)
-        imin = QLabel('мин')
+        self.intervalLayoutEdit = QLineEdit('10')
+        self.intervalLayoutEdit.setValidator(MyIntValidator(10, 60))
+        imin = QLabel('сек')
         imin.setObjectName("secondary")
         settingsLayout.addWidget(intervalLayoutText, 1, 0)
         settingsLayout.addWidget(self.intervalLayoutEdit, 1, 1)
         settingsLayout.addWidget(imin, 1, 2)
 
         # back to main layout
-        soundCheckBox = QCheckBox('Звуковое оповещение')
-        leftLayout.addWidget(soundCheckBox)
+        self.soundCheckBox = QCheckBox('Звуковое оповещение')
+        leftLayout.addWidget(self.soundCheckBox)
 
-        saveCheckBox = QCheckBox('Автоматическое сохранение')
-        leftLayout.addWidget(saveCheckBox)
+        self.saveCheckBox = QCheckBox('Автоматическое сохранение')
+        leftLayout.addWidget(self.saveCheckBox)
 
         self.timeLabel = QLabel('00:00:00')
         self.timeLabel.setObjectName('timeLabel')
@@ -218,32 +269,35 @@ class MainWindow(QWidget):
     @QtCore.pyqtSlot(bool)
     def onButtonClick(self, toggled):
         if toggled:
+            if(self.saveCheckBox.isChecked()):
+                self.experimentData.saveIfNeeded()
+            self.experimentData.reset()
             self.startStopButton.setText('СТОП')
             self.interval_time = int(self.intervalLayoutEdit.text())
+            self.experimentLength = int(self.lengthSettingsEdit.text())
             self.reader.startListen(self.interval_time)
+            self.heartRatePlotWidget.reset()
+            self.breathRatePlotWidget.reset()
         else:
             self.startStopButton.setText('ЗАПУСК')
             self.reader.stopListen()
 
     @QtCore.pyqtSlot()
     def onSaveButtonClicked(self):
-        QFileDialog.getSaveFileName()
+        self.experimentData.saveToFile()
 
     @QtCore.pyqtSlot(float, float)
     def onRascanDataProcessed(self, chss, chd):
         self.heartRateText.setText(str(chss))
         self.breathRateText.setText(str(chd))
-        self.plotWidget.appendPoint(chss)
+        self.heartRatePlotWidget.appendPoint(chss)
+        self.breathRatePlotWidget.appendPoint(chd)
 
     def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Message',
-            "Вы уверены, что хотите выйти?", QMessageBox.Yes |
-            QMessageBox.No, QMessageBox.No)
+        if (self.saveCheckBox.isChecked()):
+            self.experimentData.saveIfNeeded()
+        event.accept()
 
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
 
 app = QApplication([])
 
@@ -258,6 +312,7 @@ stylesheet = open("stylesheet.qss").read()
 app.setStyleSheet(stylesheet)
 
 pg.setConfigOption('background', 'w')
+pg.setConfigOptions(antialias=True)
 
 mainWindow = MainWindow()
 mainWindow.show()
