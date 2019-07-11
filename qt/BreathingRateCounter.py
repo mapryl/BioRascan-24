cf500cbffe041909ec21de51b1ecbcf390678840
Alina <48862818+mapryl@users.noreply.github.com>
2019-07-11 15:01:05 +0300

    fixed bugs with rate counting

--- qt/BreathingRateCounter.py
+++ qt/BreathingRateCounter.py
@@ -5,10 +5,10 @@ from scipy.signal import butter, find_peaks
 
 """" Объявляем константы """
 fsB = 50  # частота дисретизации БРЛ
-lowcut_hb = 0.7
-highcut_hb = 2.5
-lowcut_br = 0.01
-highcut_br = 0.4
+lowFreqHearthGlobal = 0.7
+highFreqHearthGlobal = 2.5
+lowFreqBreathGlobal = 0.01
+highFreqBreathGlobal = 0.4
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -38,18 +38,19 @@ def fourier_analysis(signal1, signal2, fs, freq_low, freq_high):
     freq_max = freqs[freq_max_ind]
     return freq_max[0]
 
+
 def breath_filter(signal1, signal2):
     order = 2
-    global lowcut_br
-    global highcut_br
+    global lowFreqBreathGlobal
+    global highFreqBreathGlobal
 
-    freq_sum = fourier_analysis(signal1, signal2, fsB, lowcut_br, highcut_br)
-    lowcut_br = freq_sum - 0.1
-    highcut_br = freq_sum + 0.1
+    freq_sum = fourier_analysis(signal1, signal2, fsB, lowFreqBreathGlobal, highFreqBreathGlobal)
+    low_freq_breath = freq_sum - 0.1
+    high_freq_breath = freq_sum + 0.1
     if (freq_sum - 0.1) <= 0:
-        lowcut_br = 0.01
+        low_freq_breath = 0.01
 
-    B_br, A_br = butter_bandpass(lowcut_br, highcut_br, fsB, order=order)
+    B_br, A_br = butter_bandpass(low_freq_breath, high_freq_breath, fsB, order=order)
 
     signalfilt_r1_1 = signal.filtfilt(B_br, A_br, signal1)
     signalfilt_r1_2 = signal.filtfilt(B_br, A_br, signal2)
@@ -65,22 +66,22 @@ def signal_without_breath(sig1, sig2):
 
 def heart_filter(signal_r1_1, signal_r1_2):
     """фильтрация по широкой полосе"""
-    order = 3
-    global lowcut_hb
-    global highcut_hb
+    order = 2
+    global lowFreqHearthGlobal
+    global highFreqHearthGlobal
 
-    B_hb_w, A_hb_w = butter_bandpass(lowcut_hb, highcut_hb, fsB, order=order)
+    B_hb_w, A_hb_w = butter_bandpass(lowFreqHearthGlobal, highFreqHearthGlobal, fsB, order=order)
     signalfilt_hb_r1_1_w = signal.filtfilt(B_hb_w, A_hb_w, signal_r1_1)
     signalfilt_hb_r1_2_w = signal.filtfilt(B_hb_w, A_hb_w, signal_r1_2)
 
-    freq_sum = fourier_analysis(signal_r1_1, signal_r1_2, fsB, lowcut_hb, highcut_hb)
+    freq_sum = fourier_analysis(signal_r1_1, signal_r1_2, fsB, lowFreqHearthGlobal, highFreqHearthGlobal)
 
-    lowcut_hb = freq_sum - 0.3
-    if lowcut_hb < 0:
-        lowcut_hb = 0.7
-    highcut_hb = freq_sum + 0.4
+    low_freq_hearth = freq_sum - 0.3
+    if low_freq_hearth < 0:
+        low_freq_hearth = 0.7
+    high_freq_heath = freq_sum + 0.4
 
-    B_hb, A_hb = butter_bandpass(lowcut_hb, highcut_hb, fsB, order=order)
+    B_hb, A_hb = butter_bandpass(low_freq_hearth, high_freq_heath, fsB, order=order)
     signalfilt_hb_r1_1 = signal.filtfilt(B_hb, A_hb, signal_r1_1)
     signalfilt_hb_r1_2 = signal.filtfilt(B_hb, A_hb, signal_r1_2)
 
@@ -107,16 +108,16 @@ def breath_rate_counter(signal_r1_1, signal_r1_2, time):
     # peaks_hb_r2_1_w = find_peaks(signalfilt_hb_r2_1_w, distance=fsB / highcut_hb)[0]
     # peaks_hb_r2_2_w = find_peaks(signalfilt_hb_r2_2_w, distance=fsB / highcut_hb)[0]
 
-    peaks_hb_r1_1 = find_peaks(signalfilt_hb_r1_1, distance=fsB / highcut_hb)[0]
-    peaks_hb_r1_2 = find_peaks(signalfilt_hb_r1_2, distance=fsB / highcut_hb)[0]
+    peaks_hb_r1_1 = find_peaks(signalfilt_hb_r1_1, distance=fsB / highFreqHearthGlobal)[0]
+    peaks_hb_r1_2 = find_peaks(signalfilt_hb_r1_2, distance=fsB / highFreqHearthGlobal)[0]
 
-    peaks_br_r1_1 = find_peaks(signalfilt_br_r1_1, distance=fsB / highcut_br)[0]
-    peaks_br_r1_2 = find_peaks(signalfilt_br_r1_2, distance=fsB / highcut_br)[0]
+    peaks_br_r1_1 = find_peaks(signalfilt_br_r1_1, distance=fsB / highFreqBreathGlobal)[0]
+    peaks_br_r1_2 = find_peaks(signalfilt_br_r1_2, distance=fsB / highFreqBreathGlobal)[0]
 
     total_breath_rate = (len(peaks_br_r1_1) + len(peaks_br_r1_2)) / 2
     total_breath_rate = total_breath_rate / time * 60
 
-    total_heart_rate = (len(peaks_hb_r1_1) + len(peaks_hb_r1_2) )/ 2
+    total_heart_rate = (len(peaks_hb_r1_1) + len(peaks_hb_r1_2)) / 2
     total_heart_rate = total_heart_rate / time * 60
 
     return total_heart_rate, total_breath_rate
